0732a7c6c56eb4c5b094e226ffc97e46841d0b64
bmwayne <rohitvarmachitti@gmail.com>
2021-01-28 20:30:58 -0600

    added player movement

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -12,9 +12,10 @@ pygame.display.set_icon(icon)   #Logo of the game window
 playerimg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 470
+player_speed = 0
 
-def player():
-    screen.blit(playerimg, (playerX,playerY))
+def player(x,y):
+    screen.blit(playerimg, (x,y))
 
 game = True
 while game:
@@ -23,5 +24,14 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
-    player()
+        #Player Movement
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player_speed = -0.3
+            if event.key == pygame.K_RIGHT:
+                player_speed = 0.3
+        if event.type == pygame.KEYUP:
+            player_speed = 0
+    playerX += player_speed
+    player(playerX,playerY)
     pygame.display.update()
