9aded358b03c55be36353c0529aa44a46285858e
bmwayne <rohitvarmachitti@gmail.com>
2021-01-28 19:14:59 -0600

    added title, logo and filled the screen with color

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -1,11 +1,17 @@
 import pygame
 
-pygame.init() #initializing pygame
+pygame.init() #Initializing pygame
 
-pygame.display.set_mode((800,600))  #setting up game window size
+screen = pygame.display.set_mode((800,600))  #Setting up game window size
+pygame.display.set_caption('Space Invaders')    #Title of the game window
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)   #Logo of the game window
 
 game = True
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+
+    screen.fill((0, 58, 108))
+    pygame.display.update()
