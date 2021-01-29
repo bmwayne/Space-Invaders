aeb8a58ee339e74ad70a14521b26a41ae768229d
bmwayne <rohitvarmachitti@gmail.com>
2021-01-28 18:53:45 -0600

    added quit the game event

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -2,5 +2,10 @@ import pygame
 
 pygame.init() #initializing pygame
 
-pygame.display.set_mode((800,600))  #setting up game window
+pygame.display.set_mode((800,600))  #setting up game window size
 
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
